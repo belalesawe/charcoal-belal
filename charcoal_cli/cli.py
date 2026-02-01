dee910227292bd52ba7929bb8be74c76b1016df0
MCODE OPS <mcode-bot@modelcode.ai>
2026-02-01 09:41:58 +0000

    fix: remove references to unimplemented command groups in cli.py
    Task 3 only implements stack operation commands (upstack, downstack, stack).
    Removed imports and registrations for user and log command groups that
    don't exist on this branch.

--- charcoal_cli/cli.py
+++ charcoal_cli/cli.py
@@ -18,8 +18,6 @@ def main(ctx: click.Context, debug: bool, quiet: bool) -> None:
 from charcoal_cli.commands.upstack import upstack
 from charcoal_cli.commands.downstack import downstack
 from charcoal_cli.commands.stack import stack
-from charcoal_cli.commands.user import user
-from charcoal_cli.commands.log import log
 
 # Import subcommands
 from charcoal_cli.commands.upstack_commands.onto import onto
@@ -28,17 +26,6 @@ from charcoal_cli.commands.downstack_commands.get import get
 from charcoal_cli.commands.downstack_commands.test import test as downstack_test
 from charcoal_cli.commands.downstack_commands.track import track
 from charcoal_cli.commands.stack_commands.test import test as stack_test
-from charcoal_cli.commands.log_commands.default import default as log_default
-from charcoal_cli.commands.log_commands.short import short as log_short
-from charcoal_cli.commands.log_commands.long import long as log_long
-from charcoal_cli.commands.user_commands.editor import editor
-from charcoal_cli.commands.user_commands.pager import pager
-from charcoal_cli.commands.user_commands.tips import tips
-from charcoal_cli.commands.user_commands.branch_date import branch_date
-from charcoal_cli.commands.user_commands.branch_prefix import branch_prefix
-from charcoal_cli.commands.user_commands.branch_replacement import branch_replacement
-from charcoal_cli.commands.user_commands.restack_date import restack_date
-from charcoal_cli.commands.user_commands.submit_body import submit_body
 
 # Register subcommands to their groups
 upstack.add_command(onto)
@@ -54,30 +41,10 @@ downstack.add_command(track, name="tr")  # Alias for track
 stack.add_command(stack_test)
 stack.add_command(stack_test, name="t")  # Alias for test
 
-# Register log subcommands
-log.add_command(log_default)
-log.add_command(log_short)
-log.add_command(log_short, name="s")  # Alias for short
-log.add_command(log_long)
-log.add_command(log_long, name="l")  # Alias for long
-
-# Register user subcommands
-user.add_command(editor)
-user.add_command(pager)
-user.add_command(tips)
-user.add_command(branch_date)
-user.add_command(branch_prefix)
-user.add_command(branch_replacement)
-user.add_command(restack_date)
-user.add_command(submit_body)
-
 # Register command groups with main CLI
-main.add_command(log)
-main.add_command(log, name="l")  # Alias for log
 main.add_command(upstack)
 main.add_command(upstack, name="us")  # Alias for upstack
 main.add_command(downstack)
 main.add_command(downstack, name="ds")  # Alias for downstack
 main.add_command(stack)
 main.add_command(stack, name="s")  # Alias for stack
-main.add_command(user)
